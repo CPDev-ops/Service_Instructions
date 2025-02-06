909d90665bbbdb6625b11c9faaa87e9fd26e2d04
Alex Becci <alex.becci@bingopilar.corp>
2025-02-06 08:52:21 -0300

    fixed console

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -13,6 +13,7 @@ function HomeContent() {
     console.log(queryValue)
     const value = parseQueryToBody(queryValue)
     console.log(value)
+    console.log('FIXED')
 
     return (
         <div className="min-h-screen">
